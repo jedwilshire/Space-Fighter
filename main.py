32692e1e3abfcd7388f7f9583e361d3c81e963f2
jedwilshire <84249299+jedwilshire@users.noreply.github.com>
2021-12-16 08:05:21 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from random import randint
+
 # SCREEN CONSTANTS
 WIDTH = 600
 HEIGHT = 500
@@ -11,10 +12,12 @@ BLACK = (0, 0, 0)
 ship = Actor('ship')
 ship.centerx = WIDTH / 2
 ship.bottom = HEIGHT
+ship.alive = True
+ship.speed = 6
 meteor = Actor('meteor1')
-# meteor.centerx = WIDTH / 2 changed to 
 meteor.left = randint(0, WIDTH - meteor.width)
 meteor.top = 0
+meteor.speed = 1
 laser = Actor('laser')
 laser.x = -100
 
@@ -30,7 +33,8 @@ def draw():
     laser.draw()
 
 def update():
-    update_ship()
+    if ship.alive == True:
+        update_ship()
     update_meteor()
     update_laser()
 
@@ -41,26 +45,33 @@ def update_laser():
         laser.x = -100
 
 def update_meteor():
-    meteor.y += 1
+    meteor.y += meteor.speed
+    # if meteor touching laser, move back to top and speed up
     if meteor.colliderect(laser):
         meteor.bottom = 0
         meteor.left = randint(0, WIDTH - meteor.width)
-    
+        meteor.speed += 1
+    # kill ship if meteor at bottom or hitting ship
+    if meteor.bottom >= HEIGHT or meteor.colliderect(ship):
+        ship.image = 'explosion'
+        ship.alive = False
+        
+        
 def update_ship():
     if keymap['up']:
-        ship.y -= 4
+        ship.y -= ship.speed
         if ship.top < 0:
             ship.top = 0
     if keymap['down']:
-        ship.y += 4
+        ship.y += ship.speed
         if ship.bottom > HEIGHT:
             ship.bottom = HEIGHT
     if keymap['left']:
-        ship.x -= 4
+        ship.x -= ship.speed
         if ship.left < 0:
             ship.left = 0
     if keymap['right']:
-        ship.x += 4
+        ship.x += ship.speed
         if ship.right > WIDTH:
             ship.right = WIDTH
     
@@ -77,7 +88,7 @@ def on_key_down(key):
         keymap['left'] = True
     if key == keys.RIGHT or key == keys.D:
         keymap['right'] = True
-    if key == keys.SPACE:
+    if key == keys.SPACE and ship.alive == True:
         fire()
 
 def on_key_up(key):
